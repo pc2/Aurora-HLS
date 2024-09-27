3eca4fda8acafea59d8ed9e756a4742b30a24a62
Gerrit Pape <papeg@mail.upb.de>
2024-09-27 14:47:28 +0200

    add run:nfc_test and update deps

--- scripts/run_latency_test.sh
+++ scripts/run_latency_test.sh
@@ -11,8 +11,6 @@ then
     source env.sh
 fi
 
-exit
-
 #https://pc2.github.io/fpgalink-gui/index.html?import=%20--fpgalink%3Dn00%3Aacl1%3Ach0-n00%3Aacl1%3Ach1%20--fpgalink%3Dn00%3Aacl0%3Ach0-n00%3Aacl0%3Ach1%20--fpgalink%3Dn00%3Aacl2%3Ach0-n00%3Aacl2%3Ach1
 srun -n 1 changeFPGAlinksXilinx --fpgalink=n00:acl1:ch0-n00:acl1:ch1 --fpgalink=n00:acl0:ch0-n00:acl0:ch1 --fpgalink=n00:acl2:ch0-n00:acl2:ch1
 
