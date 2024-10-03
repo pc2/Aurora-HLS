bbd0effb85f8dbd1df888e66fee0407e41bb9db7
Gerrit Pape <papeg@mail.upb.de>
2024-10-04 00:32:30 +0200

    filter drained and not responding nodes

--- scripts/print_available_nodes.sh
+++ scripts/print_available_nodes.sh
@@ -3,7 +3,7 @@ ml fpga fpgainfo
 
 fpgainfo | while read -r line; do
     if grep -q "xilinx" <<< $line; then
-        if grep -q -v "RESERVED" <<< $line; then
+        if grep -q -v "RESERVED\|DRAIN\|NOT_RESPONDING" <<< $line; then
             echo $line | cut -d ' ' -f 1
         fi
     fi
