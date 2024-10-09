b2c7ba93de599bed35ed932879c790a5ebe953f2
Gerrit Pape <papeg@mail.upb.de>
2024-10-09 15:04:57 +0200

    loopback per port

--- scripts/run_N1.sh
+++ scripts/run_N1.sh
@@ -13,7 +13,7 @@ fi
 
 srun -n 1 ./scripts/reset.sh
 
-#https://pc2.github.io/fpgalink-gui/index.html?import=%20--fpgalink%3Dn00%3Aacl1%3Ach0-n00%3Aacl1%3Ach1%20--fpgalink%3Dn00%3Aacl0%3Ach0-n00%3Aacl0%3Ach1%20--fpgalink%3Dn00%3Aacl2%3Ach0-n00%3Aacl2%3Ach1
-srun -n 1 changeFPGAlinksXilinx --fpgalink=n00:acl1:ch0-n00:acl1:ch1 --fpgalink=n00:acl0:ch0-n00:acl0:ch1 --fpgalink=n00:acl2:ch0-n00:acl2:ch1
+#https://pc2.github.io/fpgalink-gui/index.html?import=%20--fpgalink%3Dn00%3Aacl0%3Ach0-n00%3Aacl0%3Ach0%20--fpgalink%3Dn00%3Aacl0%3Ach1-n00%3Aacl0%3Ach1%20--fpgalink%3Dn00%3Aacl1%3Ach0-n00%3Aacl1%3Ach0%20--fpgalink%3Dn00%3Aacl1%3Ach1-n00%3Aacl1%3Ach1%20--fpgalink%3Dn00%3Aacl2%3Ach0-n00%3Aacl2%3Ach0%20--fpgalink%3Dn00%3Aacl2%3Ach1-n00%3Aacl2%3Ach1
+srun -n 1 changeFPGAlinksXilinx --fpgalink=n00:acl0:ch0-n00:acl0:ch0 --fpgalink=n00:acl0:ch1-n00:acl0:ch1 --fpgalink=n00:acl1:ch0-n00:acl1:ch0 --fpgalink=n00:acl1:ch1-n00:acl1:ch1 --fpgalink=n00:acl2:ch0-n00:acl2:ch0 --fpgalink=n00:acl2:ch1-n00:acl2:ch1
 
 srun -n 6 -l ./host_aurora_hls_test $@
