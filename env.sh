40cfb6dec8df7d15286180bdb9a23ff769ec58d9
Gerrit Pape <papeg@mail.upb.de>
2024-10-20 10:17:44 +0200

    use git lfs for csv files

--- env.sh
+++ env.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/bash
 module reset
-ml fpga devel toolchain lib && ml xilinx/xrt/2.15 zmqpp CMake/3.23.1-GCCcore-11.3.0 Boost/1.79.0-GCC-11.3.0 foss/2022a changeFPGAlinks
+ml fpga devel toolchain lib tools && ml xilinx/xrt/2.15 zmqpp CMake/3.23.1-GCCcore-11.3.0 Boost/1.79.0-GCC-11.3.0 foss/2022a changeFPGAlinks git-lfs
